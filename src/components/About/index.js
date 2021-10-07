4e201f672c0a16a4b964eb3cf034f50cca9f053b
Kayla Dombrosky <kdombroskyy@live.com>
2021-10-07 14:14:18 -0400

    heading overlay blob

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div className="container container-space" id="about">
+        <div className="container space-top" id="about">
             <p className="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
             </p>
             <p className="lead">Skills: React, GraphQL, MongoDB, Mongoose, Express.js, Node.js, OAuth, Redux, IndexDB, REST API, HTML5, JavaScript, CSS, Bootstrap, Materialize, Tailwind CSS</p>
