ac837af265e86f5d6fda28baabb1649e88879e8c
Kayla Dombrosky <kdombroskyy@live.com>
2021-10-07 23:35:24 -0400

    add contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,59 @@
 import React from 'react'; 
+import { useForm, ValidationError } from '@formspree/react';
 
 function Contact() {
+    const [state, handleSubmit] = useForm("xpzknkey");
+
+    if (state.succeeded) {
+        return (
+            <div className="container" id="contact">
+                <div className="row align-items-center">
+                    <div className="col-4">
+                        <div className="position-relative">
+                            <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" className="blob-container">
+                            <path fill="#D0E2FF" d="M50.1,-19.2C64,-4.7,74,21.9,65,38.5C56,55,28,61.6,0.3,61.4C-27.3,61.2,-54.6,54.3,-68,35.2C-81.4,16.1,-80.8,-15.1,-67.2,-29.4C-53.5,-43.7,-26.8,-41.1,-4.4,-38.6C18,-36.1,36.1,-33.6,50.1,-19.2Z" transform="translate(100 100)" />
+                            </svg>
+
+                            <h1 className="overlay-container display-6"><strong>Contact Me</strong></h1>
+                        </div>
+                    </div>
+                    <div className="col-8 "></div>
+                </div>
+
+
+                <div className="container mb-5">
+                    <div className="row">
+                        <div className="col-6">
+                            <p className="lead text-center pb-3">Interested in working on a project? Shoot me an e-mail.</p>
+
+                            <p className="lead text-center pb-3">Thanks for your message! I will be in contact shortly.</p>
+                        </div>
+
+                        <div className="col-6">
+                            <p className="lead text-center pb-3">You may also find me around here</p>
+                            <div className="d-flex justify-content-center align-items-center">
+                                <p><a href="https://github.com/kdombrosky" target="_blank" rel="noreferrer">
+                                    <i className="fab fa-github-square fa-5x text-purple mx-2"></i>
+                                </a></p>
+                                <p><a href="https://www.linkedin.com/in/kdombrosky/" target="_blank" rel="noreferrer">
+                                    <i className="fab fa-linkedin fa-5x text-purple mx-2"></i>
+                                </a></p>
+
+                                <p><a href="https://www.linkedin.com/in/kdombrosky/" target="_blank" rel="noreferrer">
+                                    <i className="fab fa-twitter-square fa-5x text-purple mx-2"></i>
+                                </a></p>
+                            </div>
+
+                            <div className="d-flex justify-content-center align-items-center">
+                                <a href="https://drive.google.com/file/d/1LwwFUGpWUvVmZHcS8V9QLl-D-fIPxwH_/view" target="_blank" rel="noreferrer" className="lead" id="resume">Online resume here</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
     return (
     <div className="container" id="contact">
         <div className="row align-items-center">
@@ -17,13 +70,76 @@ function Contact() {
             <div className="col-8 "></div>
         </div>
 
-        <div className="container">
-            <p>GitHub</p>
-            <p>LinkedIn</p>
-            <p>Twitter</p>
-            <p>E-mail</p>
-        </div>
+        <div className="container mb-5">
+            <div className="row">
+                <div className="col-6">
+                    <p className="lead text-center">Interested in working on a project? Shoot me an e-mail.</p>
 
+                    <form onSubmit={handleSubmit} className="form-container">
+                        <div className="form-group">
+                            <label htmlFor="email" className="text-monospace">
+                                Email Address
+                            </label>
+                            <input
+                                id="email"
+                                type="email" 
+                                name="email"
+                                placeholder="Your email"
+                                className="form-control mb-2 "
+                            />
+                            <ValidationError 
+                                prefix="Email" 
+                                field="email"
+                                errors={state.errors}
+                            />
+                        </div>
+
+                        <div className="form-group mb-2">
+                            <label htmlFor="message">
+                                Message
+                            </label>
+                            <textarea
+                                id="message"
+                                name="message"
+                                rows="4"
+                                placeholder="Enter message"
+                                className="form-control"
+                            />
+                            <ValidationError 
+                                prefix="Message" 
+                                field="message"
+                                errors={state.errors}
+                            />
+                        </div>
+
+                        <button type="submit" disabled={state.submitting} className="btn bg-purple text-dark btn-lg btn-block">
+                            Submit
+                        </button>
+                    </form>
+                </div>
+
+                <div className="col-6">
+                    <p className="lead text-center pb-3">You may also find me around here</p>
+                    <div className="d-flex justify-content-center align-items-center">
+                        <p><a href="https://github.com/kdombrosky" target="_blank" rel="noreferrer">
+                            <i className="fab fa-github-square fa-5x text-dark mx-2"></i>
+                        </a></p>
+                        <p><a href="https://www.linkedin.com/in/kdombrosky/" target="_blank" rel="noreferrer">
+                            <i className="fab fa-linkedin fa-5x text-dark mx-2"></i>
+                        </a></p>
+
+                        <p><a href="https://www.linkedin.com/in/kdombrosky/" target="_blank" rel="noreferrer">
+                            <i className="fab fa-twitter-square fa-5x text-dark mx-2"></i>
+                        </a></p>
+
+                        <p><a href="https://www.linkedin.com/in/kdombrosky/" target="_blank" rel="noreferrer">
+                            <i className="far fa-file-alt fa-5x text-dark mx-2"></i>
+                        </a></p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
     );
 }
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,18 +4,18 @@ import logo from '../../assets/favicon-32x32.png';
 function Footer() {
     return (
     <footer className=" bg-dark text-center text-lg-start text-muted">
-        <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+        <section className="d-flex justify-content-center justify-content-lg-between p-4">
     
             <div className="me-5 d-none d-lg-block"><img src={logo} alt=""></img> <strong className="px-2">2021</strong> </div>
-
+            <div className="me-5 d-none d-lg-block">Thanks for visiting! </div>
             <div>
-            <a href="" className="me-4 text-reset">
+            <a href="https://github.com/kdombrosky" target="_blank" rel="noreferrer" className="me-4 text-reset">
                 <i className="fab fa-github"></i>
             </a>
-            <a href="" className="me-4 text-reset">
+            <a href="https://www.linkedin.com/in/kdombrosky/" target="_blank" rel="noreferrer" className="me-4 text-reset">
                 <i className="fab fa-linkedin"></i>
             </a>
-            <a href="" className="me-4 text-reset">
+            <a href="mailto: kdombrosky@gmail.com" target="_blank" rel="noreferrer" className="me-4 text-reset">
                 <i className="fab fa-google"></i>
             </a>
             <a href="https://twitter.com/kae_aii" target="_blank" rel="noreferrer" className="me-4 text-reset">
@@ -28,3 +28,4 @@ function Footer() {
 }
 
 export default Footer;
+
